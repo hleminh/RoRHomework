abc8e7b0c604d47ea2e885763e55b6adb9d03ac9
yungwiche <le.minhhoang@outlook.com>
2017-09-26 00:49:52 +0700

    Homework 1.

---
+++ homework1/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Homework1::Application.config.secret_key_base = 'dfd96f5c3cf9ef463ad3aceef30a037b613bb930810a6ca30215f59ccb5c17fac5a533deca2c457687769e332656e0adcc6c99eb0b22fd0e2a5d4603309a9b80'
